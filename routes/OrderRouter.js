e9232d17cbb257ec909fdd852fc54975c1195608
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2023-12-21 21:58:23 +0700

    adding smth

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
-// const orderController = require("../controller/OrderController");
+const orderController = require("../controller/OrderController");
 const { validateAdmin, validateUser } = require("../validator/UsersValidator");
 
 //For the MongoDB database -- Start Here
-// router.post("/create", validateUser, orderController.createOrder);
-// router.get("/getall", validateUser, orderController.getAllOrder);
-// router.delete("/delete/:orderId", validateAdmin, orderController.deleteById);
+router.post("/create", orderController.createOrder);
+router.get("/getAll", orderController.getAllOrder);
+router.delete("/delete/:orderID", validateAdmin, orderController.deleteById);
 //For the MongoDB database -- End Here
 
 
